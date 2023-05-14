20da86bc5263038f712f9264e45593d895b72d2c
Diego Morales <mor21146@uvg.edu.gt>
2023-05-14 16:55:18 -0600

    eslint

--- spotify/.eslintrc.cjs
+++ spotify/.eslintrc.cjs
@@ -1,15 +1,27 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  env: {
+    browser: true,
+    es2022: true,
+    es6: true,
   },
-}
+  extends: [
+    'plugin:react/recommended',
+    'airbnb-base',
+    'airbnb/rules/react',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", ".svg"] }],
+  },
+};
